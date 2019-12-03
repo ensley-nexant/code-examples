ba3f41d6ec26a85064225ce64563528f68bf13e0
Ensley <JEnsley@nexant.corp>
2019-12-03 15:57:40 -0700

    DST adjustment implemented

--- daylight-savings/daylight-savings-adjustment.R
+++ daylight-savings/daylight-savings-adjustment.R
@@ -7,10 +7,23 @@ library(lubridate)
 days <- seq(ymd('2019-01-01'), ymd('2019-12-31'), by = 'day')
 hours <- 0:23
 
-interval <- crossing(days, hours) %>% 
+interval <- crossing(date = days, hr = hours) %>% 
   mutate(usage = rnorm(n(), 100, 10))
 
 
 # adjust for DST --------------------------------------------------------------------------------------------------
 
-# TODO
+dst_start <- ymd('2019-03-17')
+dst_end <- ymd('2019-11-03')
+dst_hr <- 3
+
+is_daylight_savings <- function(df) {
+  is_dst <- ((df$date > dst_start) | (df$date == dst_start & df$hr >= dst_hr)) &
+            ((df$date < dst_end) | (df$date == dst_end & df$hr < dst_hr))
+  
+  is_dst
+}
+
+interval <- interval %>% 
+  mutate(hr_dst = if_else(is_daylight_savings(interval), (hr + 1L) %% 24L, hr),
+         date_dst = if_else(hr != hr_dst & hr_dst == 0, date + days(1), date))
